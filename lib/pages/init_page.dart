ef13200df0de40e8ca188fc67cf62abc8446c240
elyon <elyonccari7692@gmail.com>
2023-11-16 10:49:17 -0500

    YOUTUBE_ELYON

---
+++ lib/pages/init_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class initPage extends StatelessWidget {
+  const initPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
